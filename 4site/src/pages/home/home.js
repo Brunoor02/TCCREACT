bdf4189ce65d12a2d26d9032dcdefbe4c84de407
Bruno <bruno.virgilio06@gmail.com>
2022-06-20 22:37:07 -0300

    TUDO FEITO eu acho

--- 4site/src/pages/home/home.js
+++ 4site/src/pages/home/home.js
@@ -12,9 +12,9 @@ import '../../common.scss'
 
 
 
-export default function Home (){
-    return(
-        <main>
+export default function Home() {
+  return (
+    <main>
       <section className='Fx1'>
         <div className='fx1-titulo'>
 
@@ -27,7 +27,7 @@ export default function Home (){
         <img src={logo} alt="" width="200" />
 
         <div className="d-md-none btn">
-          <Link to ="/"> INICIO </Link>
+          <Link to="/"> INICIO </Link>
           <Link to="/Login"> LOGIN </Link>
         </div>
       </section>
@@ -47,80 +47,92 @@ export default function Home (){
 
           <div className="fx3-fz">
             <div className="fx3-fz2">
-              <img src={torradeira} alt="" width="45" left="25.6%" />
-              <h1>cozinha</h1>
-              <p>Armarios, gavetas, gabinetes, bancadas.Tudo para deixar sua cozinha organizada e charmosa </p>
+              <div className='tituloFz'>
+                <img src={torradeira} alt="" width="45" left="25.6%" />
+                <h1>cozinha</h1>
+              </div>
+              <div>
+                <p>Armarios, gavetas, gabinetes, bancadas.Tudo para deixar sua cozinha organizada e charmosa </p>
+              </div>
             </div>
             <div className="fx3-fz2">
-              <img src={cama} alt="" width="60" className="fx3-img3" />
-              <h1>quartos</h1>
+              <div className='tituloFz'>
+                <img src={cama} alt="" width="60" className="fx3-img3" />
+                <h1>quartos</h1>
+              </div>
               <p>Camas, guarda-roupas, sapateira, gavateiras, etc. Moveis para deixar o seu sono mais aconchegantes</p>
             </div>
           </div>
           <div className="fx3-fz">
             <div className="fx3-fz2">
-              <img src={chuveiro} alt="" className="fx3-img" width="60" left="20%" />
-              <h1>banheiros</h1>
-              <p>Gabinetes, armarios, suportes. Tudo para manter o seu local de banho ais arrumado</p>
+              <div className='tituloFz'>
+                <img src={chuveiro} alt="" className="fx3-img" width="60" left="20%" />
+                <h1>banheiros</h1>
+              </div>
+              <div>
+                <p>Gabinetes, armarios, suportes. Tudo para manter o seu local de banho ais arrumado</p>
+              </div>
             </div>
             <div className="fx3-fz2">
-              <img src={escritorio} alt="" width="40" className="fx3-img2" />
-              <h1>escritorio</h1>
+              <div className='tituloFz'>
+                <img src={escritorio} alt="" width="40" className="fx3-img2" />
+                <h1>escritorio</h1>
+              </div>
               <p>Escrivaninha, aramrios, gavetas, estantes. Mantenha o seu escritorio arrumado para você trabalhar bem</p>
             </div>
           </div>
         </div>
       </section>
       <section className="fx4">
-                <div className="fx4-ligue">
-                    <h1 className="fx4-orça">Venha fazer um orçamento conosco</h1>
-                    <button className="fx4-btn">*Contato no rodapé*</button>
-                </div>
-                <div className="fx4-sobre">
-                    <h2>sobre</h2>
-                    <p>Uma verdadeira devoção ao ofício resulta da satisfação de transformar ideias em realidade. Fundei o(a) Marcenaria Power em ano X como uma plataforma para mostrar e vender produtos artesanais de qualidade. Às vezes, no mundo prático de hoje, podemos ignorar a beleza natural e artesanal. Entre em contato para iniciar seu projeto hoje.
-                    </p>
-                </div>    
-    </section>
-    <section className="fx5">
+        <div className="fx4-ligue">
+          <h1 className="fx4-orça">Venha fazer um orçamento conosco</h1>
+          <button className="fx4-btn">*Contato no rodapé*</button>
+        </div>
+        <div className="fx4-sobre">
+          <h2>sobre</h2>
+          <p>Uma verdadeira devoção ao ofício resulta da satisfação de transformar ideias em realidade. Fundei o(a) Marcenaria Power em ano X como uma plataforma para mostrar e vender produtos artesanais de qualidade. Às vezes, no mundo prático de hoje, podemos ignorar a beleza natural e artesanal. Entre em contato para iniciar seu projeto hoje.
+          </p>
+        </div>
+      </section>
+      <section className="fx5">
         <h1>Alguns dos nossos produtos.</h1>
         <div className="fx5-p1">
-            <div className="fx5-img">
-                <img src={m1} alt="" width="300"/>
-            </div>
-            <div className="fx5-img">
-                <img src={m1} alt="" width="300"/>
-            </div>
-            <div className="fx5-img">
-                <img src={m1} alt="" width="300"/>
-            </div>
+          <div className="fx5-img">
+            <img src={m1} alt="" width="300" />
+          </div>
+          <div className="fx5-img">
+            <img src={m1} alt="" width="300" />
+          </div>
+          <div className="fx5-img">
+            <img src={m1} alt="" width="300" />
+          </div>
         </div>
         <div className="fx5-p2">
-            <div className="fx5-img">
-                <img src={m1} alt="" width="300"/>
-            </div>
-            <div className="fx5-img">
-                <img src={m1} alt="" width="300"/>
-            </div>
-            <div className="fx5-img">
-                <img src={m1} alt="" width="300"/>
-            </div>
+          <div className="fx5-img">
+            <img src={m1} alt="" width="300" />
+          </div>
+          <div className="fx5-img">
+            <img src={m1} alt="" width="300" />
+          </div>
+          <div className="fx5-img">
+            <img src={m1} alt="" width="300" />
+          </div>
         </div>
-    </section>
-    <section className="fx6">
-        <img src={logo} alt=""/>
+      </section>
+      <section className="fx6">
+        <img src={logo} alt="" />
         <div>
-            <p className="fx6-local">Rua Natal 210 - Diadema. São Paulo - SP CEP 09910-220</p>
-            <p className="fx6-nome">Marcenaria Power</p>
+          <p className="fx6-local">Rua Natal 210 - Diadema. São Paulo - SP CEP 09910-220</p>
+          <p className="fx6-nome">Marcenaria Power</p>
         </div>
         <div className="fx6-info">
-            <p>(11) 90000-0000</p>
-            <p>(11) 1111-1111</p>
-            <p>@instamarceneiro</p>
-            <p>email@orçamento.com</p>
+          <p>(11) 90000-0000</p>
+          <p>(11) 1111-1111</p>
+          <p>@instamarceneiro</p>
+          <p>email@orçamento.com</p>
         </div>
-    </section>
+      </section>
 
     </main>
-    )
+  )
 }
